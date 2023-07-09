8e8eceaa8cc6e605e2590b59ef8c1ea8a00fcaa3
sokunsamnang <sokunsamnang45@gmail.com>
2023-07-10 02:39:50 +0700

    add google analytic and sentry key

--- src/setup/constants.ts
+++ src/setup/constants.ts
@@ -1,6 +1,6 @@
 export const APP_VERSION = import.meta.env.PACKAGE_VERSION;
 export const DISCORD_LINK = "https://discord.gg/Jhqt4Xzpfb";
 export const GITHUB_LINK = "https://github.com/movie-web/movie-web";
-export const GA_ID = "G-44YVXRL61C";
+export const GA_ID = "G-HKZRBE16R6";
 export const SENTRY_DSN =
-  "https://b267ab7d52674c23af4e4e6cf2956251@o4505053491167232.ingest.sentry.io/4505053495296000";
+  "https://2a3d982c63f84e0f96a0a8695e97af78@o4505501127344128.ingest.sentry.io/4505501128523776";
